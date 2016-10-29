0d4943fb0062a10b3966268bc5cfa7e07d1c3d71
ethachu19 <ethan.mak100@gmail.com>
2016-10-29 17:56:16 -0400

    Real command WallTrace

---
+++ TeamCode/src/main/java/virtualRobot/commands/WallTrace.java
@@ -0,0 +1,79 @@
+package virtualRobot.commands;
+
+import android.util.Log;
+
+import virtualRobot.AutonomousRobot;
+import virtualRobot.ExitCondition;
+import virtualRobot.PIDController;
+import virtualRobot.components.Sensor;
+
+/**
+ * Created by ethachu19 on 10/29/2016.
+ */
+
+public class WallTrace implements Command {
+    ExitCondition exitCondition;
+    Direction direction;
+    private AutonomousRobot robot;
+
+    public WallTrace() {
+        robot = Command.AUTO_ROBOT;
+        direction = Direction.FORWARD;
+    }
+
+    public void setDirection (Direction d) { direction = d; }
+
+    public Direction getDirection() { return direction; }
+
+    public void setExitCondition (ExitCondition e) {
+        exitCondition = e;
+    }
+
+    public ExitCondition getExitCondition () {
+        return exitCondition;
+    }
+
+    @Override
+    public boolean changeRobotState() throws InterruptedException {
+        double tp = 0.2;
+        boolean isInterrupted = false;
+        Sensor sonarLeft = direction == Direction.FORWARD ? robot.getSonarLeft() : robot.getSonarRight();
+        Sensor sonarRight = direction == Direction.FORWARD ? robot.getSonarRight() : robot.getSonarLeft();
+        PIDController close = new PIDController(0.008,0,0,0,13);
+        PIDController allign = new PIDController(0.012,0,0,0,0);
+        double currLeft, currRight, errClose = 0, errAllign;
+        while (!exitCondition.isConditionMet()) {
+            currLeft = sonarLeft.getValue();
+            currRight = sonarRight.getValue();
+
+            errClose = close.getPIDOutput(currLeft);
+            errAllign = allign.getPIDOutput(currLeft-currRight);
+
+            if (direction == Direction.FORWARD) {
+                robot.getLeftRotate().setPower(tp - errClose - errAllign);
+                robot.getRightRotate().setPower(tp + errClose + errAllign);
+            } else {
+                robot.getRightRotate().setPower((tp - errClose - errAllign)*-1);
+                robot.getLeftRotate().setPower((tp + errClose + errAllign)*-1);
+            }
+
+            if(Thread.currentThread().isInterrupted()) {
+                isInterrupted = true;
+                break;
+            }
+
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException ex) {
+                isInterrupted = true;
+                break;
+            }
+        }
+        return isInterrupted;
+    }
+
+    public enum Direction {
+        FORWARD,
+        BACKWARD
+    }
+}
