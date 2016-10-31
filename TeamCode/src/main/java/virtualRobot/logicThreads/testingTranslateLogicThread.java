51350eac4761f3d2098679af16495159faff8dfd
ethachu19 <ethan.mak100@gmail.com>
2016-10-31 13:01:35 -0400

    Wrote some new drivers, in the process of writing one for SRF08, LineTrace is now command Vector3f Matrix 3f

---
+++ TeamCode/src/main/java/virtualRobot/logicThreads/testingTranslateLogicThread.java
@@ -0,0 +1,38 @@
+package virtualRobot.logicThreads;
+
+import virtualRobot.ExitCondition;
+import virtualRobot.LogicThread;
+import virtualRobot.commands.Pause;
+import virtualRobot.commands.Translate;
+
+/**
+ * Created by ethachu19 on 10/11/2016.
+ */
+
+public class testingTranslateLogicThread extends LogicThread {
+    @Override
+    public void loadCommands() {
+//        ExitCondition e = new ExitCondition() {
+//            @Override
+//            public boolean isConditionMet()  {
+//                int i = 0;
+//                while (i < 2) {
+//                    try {
+//                        Thread.sleep(1000);
+//                    }
+//                    catch (Exception e) {
+//                        e.printStackTrace();
+//                    }
+//                    i++;
+//                }
+//                return true;
+//            }
+//        };
+        Translate moveBack = new Translate(300, Translate.Direction.BACKWARD, 0);
+//        moveBack.setExitCondition(e);
+
+            commands.add(moveBack);
+            commands.add(new Pause(2500));
+//            commands.add(new Translate(30, Translate.Direction.FORWARD_RIGHT, 0));
+    }
+}
