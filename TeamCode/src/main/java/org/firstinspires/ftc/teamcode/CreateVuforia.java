15246a59322033e3467fe695a587e9d6c7d8fee4
ethachu19 <ethan.mak100@gmail.com>
2016-12-20 10:57:27 -0500

    tidys up some things, adds txt of all log tags, still need to finish it

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CreateVuforia.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CreateVuforia.java
@@ -4,6 +4,8 @@ import android.util.Log;
 
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
 
+import java.util.ArrayList;
+
 import virtualRobot.GodThread;
 import virtualRobot.VuforiaLocalizerImplSubclass;
 import virtualRobot.godThreads.PIDLineFollowerGod;
@@ -15,10 +17,17 @@ import virtualRobot.godThreads.TeleopGodThread;
  */
 
 public class CreateVuforia implements Runnable {
+    static final ArrayList<Class<? extends GodThread>> exception = new ArrayList<>();
     Class<? extends GodThread> godThread;
     GodThread vuforiaEverywhere;
     Thread t;
     private boolean good = false;
+
+    static {
+        exception.add(TeleopGodThread.class);
+        exception.add(PIDLineFollowerGod.class);
+        exception.add(PIDTesterGodThread.class);
+    }
 
     public CreateVuforia(Class<? extends GodThread> g, GodThread vuforiaEverywhere, Thread t) {
         godThread = g;
@@ -30,18 +39,18 @@ public class CreateVuforia implements Runnable {
     @Override
     public void run() {
         try {
-            if (!godThread.equals(TeleopGodThread.class) ){//&& !godThread.equals(PIDTesterGodThread.class) && !godThread.equals(PIDLineFollowerGod.class)) {
+            if (!exception.contains(godThread)){//&& !godThread.equals(PIDTesterGodThread.class) && !godThread.equals(PIDLineFollowerGod.class)) {
                 VuforiaLocalizer.Parameters params = new VuforiaLocalizer.Parameters(R.id.cameraMonitorViewId);
                 params.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
                 params.vuforiaLicenseKey = "AdVGalv/////AAAAGYhiDIdk+UI+ivt0Y7WGvUJnm5cKX/lWesW2pH7gnK3eOLTKThLekYSO1q65ttw7X1FvNhxxhdQl3McS+mzYjO+HkaFNJlHxltsI5+b4giqNQKWhyKjzbYbNw8aWarI5YCYUFnyiPPjH39/CbBzzFk3G2RWIzNB7cy4AYhjwYRKRiL3k33YvXv0ZHRzJRkMpnytgvdv5jEQyWa20DIkriC+ZBaj8dph8/akyYfyD1/U19vowknmzxef3ncefgOZoI9yrK82T4GBWazgWvZkIz7bPy/ApGiwnkVzp44gVGsCJCUFERiPVwfFa0SBLeCrQMrQaMDy3kOIVcWTotFn4m1ridgE5ZP/lvRzEC4/vcuV0";
-                Log.d("lalala", "location1");
+                Log.d("VuforiaCamera", "Vuforia Initialization");
                 VuforiaLocalizerImplSubclass vuforia = new VuforiaLocalizerImplSubclass(params);
                 vuforiaEverywhere = godThread.newInstance();
                 vuforiaEverywhere.setVuforia(vuforia);
                 t = new Thread(vuforiaEverywhere);
             } else {
                 t = new Thread(godThread.newInstance());
-                Log.d("lalala", "location2");
+                Log.d("VuforiaCamera", "Only creating godThread");
             }
         } catch (InstantiationException e) {
             return;
