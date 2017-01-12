5e6f73515311f00da0ee3bf83c9b053d8ac01d5d
Mehmet Sirtalan <msirtalan@gmail.com>
2017-01-12 17:25:45 -0500

    TestTakePicture and TestIndividualMotor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestingOpModes/TakePictureTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestingOpModes/TakePictureTest.java
@@ -22,10 +22,12 @@ import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackableDefaultListener;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
 import org.firstinspires.ftc.robotcore.internal.VuforiaLocalizerImpl;
+import org.firstinspires.ftc.robotcore.internal.opengl.models.Teapot;
 import org.firstinspires.ftc.teamcode.R;
 
 import java.util.concurrent.atomic.AtomicBoolean;
 
+import virtualRobot.VuforiaLocalizerImplSubclass;
 import virtualRobot.commands.DavidClass;
 import virtualRobot.godThreads.TakePictureTestGod;
 
@@ -37,21 +39,66 @@ import virtualRobot.godThreads.TakePictureTestGod;
 public class TakePictureTest extends LinearOpMode {
     //private TakePictureTestGod tp;
     private AtomicBoolean redisLeft = new AtomicBoolean();
+
     @Override
     public void runOpMode() throws InterruptedException {
         VuforiaLocalizer.Parameters params = new VuforiaLocalizer.Parameters(R.id.cameraMonitorViewId);
-        params.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
+        params.cameraDirection = VuforiaLocalizer.CameraDirection.BACK; //Default
         params.vuforiaLicenseKey = "AcXbD9X/////AAAAGVpq1gdfDkIPp+j5hv1iV5RZXLWAWV4F7je9gks+8lHhZb6mwCj7xy9mapHP6sKO9OrPv5kVQDXhB+T+Rn7V7GUm4Ub4rmCanqv4frx8gT732qJUnTEj9POMufR9skjlXSEODbpThxrLCPqobHeAeSA5dUmUik3Rck0lcwhElw5yOBN45iklYnvC9GpPRv128ALcgt9Zpw/shit0erKmuyrT62NRUKgoHNMm5xV/Xqj8Vgwke8ESap+nK7v+6lx35vDZ6ISNDVMMM8h0VqeL0745MNPJoI1vgiNRo30R7WwtPYME44koOrWMUIxMXghtqxq7AfFxb6sbin0i5KSUJWtLsqmZOrAXxjxdUwY8f8tw";
-        params.cameraMonitorFeedback = VuforiaLocalizer.Parameters.CameraMonitorFeedback.AXES;
+        params.cameraMonitorFeedback = VuforiaLocalizer.Parameters.CameraMonitorFeedback.AXES; //Default
 
+        //VuforiaLocalizer vuforia = new VuforiaLocalizerImplSubclass(params);
         VuforiaLocalizerImplSubclass vuforia = new VuforiaLocalizerImplSubclass(params);
-        //VuforiaLocalizerImplSubclass vuforia = new VuforiaLocalizerImplSubclass(params);
-        Vuforia.setHint(HINT.HINT_MAX_SIMULTANEOUS_IMAGE_TARGETS,4);
+        Vuforia.setHint(HINT.HINT_MAX_SIMULTANEOUS_IMAGE_TARGETS, 5);
+
+        VuforiaTrackables beacons = vuforia.loadTrackablesFromAsset("Beacons"); //xml file in FtcRobotController/src/main/assets
+        VuforiaTrackables beaconsT = vuforia.loadTrackablesFromAsset("FTC_2016-17");
+
+        beacons.get(0).setName("RedIsLeftSample");
+        beacons.get(1).setName("BlueIsLeftSample");
+        beacons.get(2).setName("RedIsLeft");
+        beacons.get(3).setName("BlueIsLeft");
+        beacons.get(4).setName("RedIsLeft3D");
+
+        beaconsT.get(3).setName("Gears");
+
+        beacons.activate();
+        beaconsT.activate();
 
         waitForStart();
 
-        if (vuforia.rgb != null){
-            Bitmap bm =  Bitmap.createBitmap(vuforia.rgb.getWidth(), vuforia.rgb.getHeight(), Bitmap.Config.RGB_565);
+        while (opModeIsActive()) {
+            for (VuforiaTrackable beac : beacons) {
+                OpenGLMatrix pose = ((VuforiaTrackableDefaultListener) beac.getListener()).getPose();
+
+                if (pose != null) {
+                    VectorF translation = pose.getTranslation();
+                    telemetry.addData(beac.getName() + "-Translate", translation);
+                    //double degreesToTurn = Math.toDegrees(Math.atan2(translation.get(1), translation.get(2))); //0, 2 for landscape; 1, 2 for portrait
+                    //telemetry.addData(beac.getName() + "-Degrees", degreesToTurn);
+                }
+                else
+                    telemetry.addData("Tracking Object " + beac.getName() + ": ", false);
+            }
+
+            for (VuforiaTrackable beac : beaconsT) {
+                OpenGLMatrix pose = ((VuforiaTrackableDefaultListener) beac.getListener()).getPose();
+
+                if (pose != null) {
+                    VectorF translation = pose.getTranslation();
+                    telemetry.addData(beac.getName() + "-Translate", translation);
+                    //double degreesToTurn = Math.toDegrees(Math.atan2(translation.get(1), translation.get(2))); //0, 2 for landscape; 1, 2 for portrait
+                    //telemetry.addData(beac.getName() + "-Degrees", degreesToTurn);
+                }
+                else
+                    telemetry.addData("Tracking Object " + beac.getName() + ": ", false);
+            }
+
+            telemetry.update();
+        }
+
+        if (vuforia.rgb != null) {
+            Bitmap bm = Bitmap.createBitmap(vuforia.rgb.getWidth(), vuforia.rgb.getHeight(), Bitmap.Config.RGB_565);
             bm.copyPixelsFromBuffer(vuforia.rgb.getPixels());
             boolean analyzed = DavidClass.analyzePic2(bm);
             telemetry.addData("cameraReturn", analyzed + " ");
@@ -59,80 +106,5 @@ public class TakePictureTest extends LinearOpMode {
             Log.d("cameraReturn ", analyzed + " ");
             redisLeft.set(analyzed);
         }
-
-    }
-    public class VuforiaLocalizerImplSubclass extends VuforiaLocalizerImpl {
-
-        public Image rgb;
-
-        class CloseableFrame extends Frame {
-            public CloseableFrame(Frame other) { // clone the frame so we can be useful beyond callback
-                super(other);
-            }
-            public void close() {
-                super.delete();
-            }
-        }
-
-
-        public class VuforiaCallbackSubclass extends VuforiaLocalizerImpl.VuforiaCallback {
-
-            @Override public synchronized void Vuforia_onUpdate(State state) {
-                super.Vuforia_onUpdate(state);
-                // We wish to accomplish two things: (a) get a clone of the Frame so we can use
-                // it beyond the callback, and (b) get a variant that will allow us to proactively
-                // reduce memory pressure rather than relying on the garbage collector (which here
-                // has been observed to interact poorly with the image data which is allocated on a
-                // non-garbage-collected heap). Note that both of this concerns are independent of
-                // how the Frame is obtained in the first place.
-                CloseableFrame frame = new CloseableFrame(state.getFrame());
-                RobotLog.vv(TAG, "received Vuforia frame#=%d", frame.getIndex());
-
-                //Important Stuff:
-                long num = frame.getNumImages();
-
-                for(int i = 0; i < num; i++){
-                    if(frame.getImage(i).getFormat() == PIXEL_FORMAT.RGB565){
-                        rgb = frame.getImage(i);
-                    }
-                }
-                //End important stuff.
-
-                frame.close();
-            }
-        }
-
-        public VuforiaLocalizerImplSubclass(VuforiaLocalizer.Parameters parameters) {
-            super(parameters);
-            stopAR();
-            clearGlSurface();
-
-            this.vuforiaCallback = new VuforiaCallbackSubclass();
-            startAR();
-
-            // Optional: set the pixel format(s) that you want to have in the callback
-            Vuforia.setFrameFormat(PIXEL_FORMAT.RGB565, true);
-        }
-
-        public void clearGlSurface() {
-            if (this.glSurfaceParent != null) {
-                appUtil.synchronousRunOnUiThread(new Runnable() {
-                    @Override public void run() {
-                        glSurfaceParent.removeAllViews();
-                        glSurfaceParent.getOverlay().clear();
-                        glSurface = null;
-                    }
-                });
-            }
-        }
     }
 }
-/*
-    public void init(){
-        tp = new TakePictureTestGod();
-    }
-
-    public void loop(){
-        telemetry.addData("redIsLeft: ", tp.getRedIsLeft());
-    }*/
-
